cfebde3d91d7a0b8f337d7511e0bd64c52243b76
Mike Lofgren <mikeylofgren@gmail.com>
2020-11-11 20:45:08 -0800

    add in additional routes.

---
+++ src/pages/company/team.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Layout from "../../components/layout"
+
+const Team = () => {
+  return (
+    <Layout>
+  <div>
+    <h1>The Team</h1>
+    <p>
+      Art party stumptown etsy readymade sartorial tbh ramps edison bulb activated charcoal iceland copper mug. Mumblecore gentrify vinyl irony jianbing tumblr. Meggings kombucha williamsburg bicycle rights health goth mlkshk flannel. Trust fund cold-pressed crucifix you probably haven't heard of them beard la croix poutine readymade lyft lo-fi. Cloud bread affogato narwhal cray, crucifix craft beer kombucha ennui dreamcatcher tattooed kale chips.
+    </p>
+  </div>
+    </Layout>
+  )
+}
+
+export default Team
